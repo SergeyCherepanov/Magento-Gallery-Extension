5df903ea7785d79abc0e6400d9fae15e26699c23
SergeyCherepanov <sergey@cherepanov.org.ua>
2014-08-07 21:44:28 +0300

    Frontend Tempaltes, Controllers and Blocks

--- app/code/community/Open/Gallery/controllers/IndexController.php
+++ app/code/community/Open/Gallery/controllers/IndexController.php
@@ -4,20 +4,11 @@ class Open_Gallery_IndexController
     extends Mage_Core_Controller_Front_Action
 {
     /**
-     * Prepare View Page
+     * Gallery Home
      */
-    public function viewAction()
+    public function indexAction()
     {
-        /** @var Open_Gallery_Model_Item $item */
-        $item = Mage::getModel('open_gallery/item');
-        $item->load($this->getRequest()->getParam('id'));
-
-        Mage::register('gallery_item', $item);
-
-        if ($item->getId()) {
-            $item->getHelper()->prepareAndRenderView($item, $this);
-        } else {
-            $this->_redirectReferer();
-        }
+        $this->loadLayout();
+        $this->renderLayout();
     }
 }
